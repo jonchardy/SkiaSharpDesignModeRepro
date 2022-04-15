b715882db00c9cf25e8877ee0976bc11908b93cd
Jon Hardy <jonathan.c.hardy@gmail.com>
2022-04-15 10:37:04 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # skiasharp-repro
  
+When using SkiaSharp.Views.WindowsForms, one cannot create SkiaSharp classes (in this case, SKFont) within the constructor without breaking Visual Studio's design mode.
+
+1) Clone repository.
+2) Open SkiaSharpRepro.sln in Visual Studio 2022.
+3) Right click SkiaSharpReproLib -> Pack.
+4) Move generated .nupkg to a local NuGet package source.
+5) Right click WindowsFormsApp1 -> Manage NuGet Packages...
+6) Install the SkiaSharpReproLib package from the local package source.
+7) Drag and drop SkiaSharpReproLib/MyCustomControl from the toolbox onto Form1.
+
+An exception occurs:
+```
+Failed to create component 'MyCustomControl'.  The error message follows:
+'System.DllNotFoundException: Unable to load library 'libSkiaSharp'.
+...'
+```
---
+++ SkiaSharpReproLib/MyCustomControl.cs
@@ -0,0 +1,39 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkiaSharpReproLib
+{
+    [ToolboxItem(true)]
+    public partial class MyCustomControl : SKControl
+    {
+        private SKFont myFont;
+
+        public MyCustomControl()
+        {
+            InitializeComponent();
+
+            myFont = new SKFont
+            {
+                Typeface = SKTypeface.FromFamilyName("Segoe UI"),
+                Subpixel = true
+            };
+        }
+
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            base.OnPaintSurface(e);
+            var c = e.Surface.Canvas;
+            c.Clear();
+            c.DrawText("Test", new SKPoint(20, 20), new SKPaint());
+        }
+    }
+}
